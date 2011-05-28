173d42d2e1971065d0eca4d919be652bee871daa
Leif Johnson <leif@leifjohnson.net>
2011-05-27 22:02:26 -0500

    Get mnist (image) test into pretty good working order. Still slow, even using the C module ... :-/

--- test/mnist.py
+++ test/mnist.py
@@ -39,27 +39,29 @@ import lmj.pursuit
 FLAGS = optparse.OptionParser()
 FLAGS.add_option('', '--model')
 
-FLAGS.add_option('-b', '--batch-size', type=int, default=10)
+FLAGS.add_option('-b', '--batch-size', type=int, default=1)
 FLAGS.add_option('-f', '--frames', action='store_true')
 FLAGS.add_option('-m', '--min-coeff', type=float, default=0.1)
-FLAGS.add_option('-x', '--max-num-coeffs', type=int, default=1000)
+FLAGS.add_option('-x', '--max-num-coeffs', type=int, default=-1)
 FLAGS.add_option('-a', '--alpha', type=float, default=0.1)
-FLAGS.add_option('-t', '--tau', type=float, default=0.999)
-FLAGS.add_option('-r', '--rows', type=int, default=5)
-FLAGS.add_option('-c', '--cols', type=int, default=5)
-FLAGS.add_option('-W', '--width', type=int, default=400)
-FLAGS.add_option('-H', '--height', type=int, default=400)
+FLAGS.add_option('-t', '--tau', type=float, default=1.)
+FLAGS.add_option('-r', '--rows', type=int, default=10)
+FLAGS.add_option('-c', '--cols', type=int, default=10)
 
 FLAGS.add_option('', '--l1', type=float, default=0.)
 FLAGS.add_option('', '--l2', type=float, default=0.)
 FLAGS.add_option('', '--momentum', type=float, default=0.)
 FLAGS.add_option('', '--padding', type=float, default=0.1)
-FLAGS.add_option('', '--grow', type=float, default=0.15)
-FLAGS.add_option('', '--shrink', type=float, default=0.015)
+FLAGS.add_option('', '--grow', type=float, default=0.1)
+FLAGS.add_option('', '--shrink', type=float, default=0.01)
 
 
 def now():
     return datetime.datetime.now()
+
+
+def load_image(path):
+    return numpy.asarray(Image.open(path).convert('L')) / 128. - 1.
 
 
 def save_frame(width, height):
@@ -67,20 +69,6 @@ def save_frame(width, height):
     Image.fromstring(mode="RGB", size=(width, height), data=pixels
                      ).transpose(Image.FLIP_TOP_BOTTOM
                                  ).save(now().strftime('frame-%Y%m%d-%H%M%S.%f.png'))
-
-
-def load_image(path):
-    a = numpy.asarray(Image.open(path).convert('L'))
-    return (a - 128.) / 128.
-
-
-def mean(x):
-    return sum(x) / max(1, len(x))
-
-
-def choose(a):
-    '''Get a random element from sequence a.'''
-    return a[rng.randint(0, len(a) - 1)]
 
 
 if __name__ == '__main__':
@@ -99,7 +87,7 @@ if __name__ == '__main__':
     batches = 0.
     devset = []
     for _ in range(min(len(args) // 2, 10)):
-        arg = choose(args)
+        arg = args[rng.randint(0, len(args) - 1)]
         devset.append(load_image(arg))
         args.remove(arg)
     trainset = [load_image(arg) for arg in args]
@@ -129,21 +117,23 @@ if __name__ == '__main__':
     filters = numpy.zeros((opts.rows, opts.cols, 20, 20), 'f')
     features = numpy.zeros((opts.rows, opts.cols, width, height), 'f')
 
-    kwargs = dict(cmap=glumpy.colormap.Grey, vmin=0, vmax=255)
+    kwargs = dict(cmap=glumpy.colormap.Grey, vmin=-1, vmax=1)
     _source = glumpy.Image(source, **kwargs)
     _reconst = glumpy.Image(reconst, **kwargs)
-    _filters = [[glumpy.Image(f, vmin=-0.3, vmax=0.3) for f in fs] for fs in filters]
-    _features = [[glumpy.Image(f, vmin=0, vmax=2) for f in fs] for fs in features]
+    _filters = [[glumpy.Image(f, vmin=-0.2, vmax=0.2) for f in fs] for fs in filters]
+    _features = [[glumpy.Image(f, vmin=0, vmax=3) for f in fs] for fs in features]
 
     W = 60 * (2 * opts.cols + 1) + 4
     H = 60 * opts.rows + 4
 
     win = glumpy.Window(W, H)
 
-    def learn():
-        global batches
-        batch = [choose(trainset) for _ in range(opts.batch_size)]
+    def update():
+        global batches, filters, _filters
+
         batches += 1
+        batch = [trainset[rng.randint(0, len(trainset) - 1)]
+                 for _ in range(opts.batch_size)]
 
         start = time.time()
         Grad = None
@@ -160,28 +150,33 @@ if __name__ == '__main__':
             learning_rate=opts.alpha * opts.tau ** batches)
         logging.info('processed batch in %dms', 1000 * (time.time() - start))
 
-        return pixels
-
-    def update(pixels):
-        global filters, _filters
-
         enc = codebook.encode(pixels.copy(),
                               max_num_coeffs=opts.max_num_coeffs,
                               min_coeff=opts.min_coeff)
 
+        estimate = codebook.decode(enc, pixels.shape)
+
+        errors.append(abs(pixels - estimate).sum())
+        logging.error('%d<%.3g>: error %d',
+                      batches,
+                      opts.alpha * opts.tau ** batches,
+                      sum(errors) / max(1, len(errors)))
+
         w, h = pixels.shape
 
         source[:] = 0.
-        source[:w, :h] = pixels * 128 + 128
+        source[:w, :h] = pixels
         _source.update()
 
         reconst[:] = 0.
-        reconst[:w, :h] = codebook.decode(enc, pixels.shape) * 128 + 128
+        reconst[:w, :h] = estimate
         _reconst.update()
 
         features[:] = 0
-        for i, c, o in enc:
-            features[numpy.unravel_index(i, (opts.rows, opts.cols)) + o] += c
+        for i, c, (x, y) in enc:
+            w, h = codebook.filters[i].shape
+            a, b = numpy.unravel_index(i, (opts.rows, opts.cols))
+            features[a, b, x:x+w, y:y+h] += c
         [[f.update() for f in fs] for fs in _features]
 
         # re-allocate new filter arrays if filters have grown too much.
@@ -205,35 +200,36 @@ if __name__ == '__main__':
         for t, pixels in enumerate(devset):
             estimate = trainer.reconstruct(pixels.copy())
             errors.append(abs(pixels - estimate).sum())
-        logging.error('%d<%.3g>: error %d', batches, opts.alpha * opts.tau ** batches, mean(errors))
+        logging.error('%d<%.3g>: error %d',
+                      batches,
+                      opts.alpha * opts.tau ** batches,
+                      sum(errors) / max(1, len(errors)))
 
     @win.event
     def on_draw():
         win.clear()
-        p = 4
         W, H = win.get_size()
-        w = int(float(W - p) / (2 * opts.cols + 1))
-        h = int(float(H - p) / opts.rows)
-        _source.blit(w * opts.cols + p, p, w - p, h - p)
-        _reconst.blit(w * opts.cols + p, h + p, w - p, h - p)
+        p = 4
+        w = int(float(W - p) / (2 * opts.cols))
+        h = int(float(H - p) / (2 * opts.rows))
+        _source.blit(p, h * opts.rows + p, w * opts.cols - p, h * opts.rows - p)
+        _reconst.blit(w * opts.cols + p, h * opts.rows + p, w * opts.cols - p, h * opts.rows - p)
         for r in range(opts.rows):
             fs = _filters[r]
             Fs = _features[r]
             for c in range(opts.cols):
                 fs[c].blit(w * c + p, h * r + p, w - p, h - p)
-                Fs[c].blit(w * (c + 1 + opts.cols) + p, h * r + p, w - p, h - p)
+                Fs[c].blit(w * (c + opts.cols) + p, h * r + p, w - p, h - p)
 
     @win.event
     def on_idle(dt):
         global updates
+        win.draw()
         if updates != 0:
             updates -= 1
-            update(learn())
-            win.draw()
+            update()
             if opts.frames:
                 save_frame(W, H)
-            if rng.random() < 0.1:
-                evaluate()
 
     @win.event
     def on_key_press(key, modifiers):
