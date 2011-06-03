2e7327001b45cb890bd11498a697f07ec8042f6e
Leif Johnson <leif@leifjohnson.net>
2011-06-03 18:42:49 -0500

    Rework the image test/vis as a class, updating after each encoding step for a more responsive gui.

--- test/images.py
+++ test/images.py
@@ -39,8 +39,7 @@ import lmj.pursuit
 FLAGS = optparse.OptionParser()
 FLAGS.add_option('', '--model')
 
-FLAGS.add_option('-b', '--batch-size', type=int, default=3)
-FLAGS.add_option('-c', '--min-coeff', type=float, default=0.1)
+FLAGS.add_option('-c', '--min-coeff', type=float, default=0.0001)
 FLAGS.add_option('-n', '--max-num-coeffs', type=int, default=-1)
 FLAGS.add_option('-a', '--min-activity-ratio', type=float, default=0.)
 
@@ -48,13 +47,9 @@ FLAGS.add_option('-R', '--rows', type=int, default=10)
 FLAGS.add_option('-C', '--cols', type=int, default=10)
 
 FLAGS.add_option('', '--learning-rate', type=float, default=0.1)
-FLAGS.add_option('', '--learning-decay', type=float, default=0.99)
-FLAGS.add_option('', '--l1', type=float, default=0.)
-FLAGS.add_option('', '--l2', type=float, default=0.)
-FLAGS.add_option('', '--momentum', type=float, default=0.)
 FLAGS.add_option('', '--padding', type=float, default=0.1)
-FLAGS.add_option('', '--grow', type=float, default=0.1)
-FLAGS.add_option('', '--shrink', type=float, default=0.001)
+FLAGS.add_option('', '--grow', type=float, default=0.01)
+FLAGS.add_option('', '--shrink', type=float, default=0.0001)
 
 FLAGS.add_option('-s', '--save-frames', action='store_true')
 
@@ -64,7 +59,7 @@ def now():
 
 
 def load_image(path):
-    return (numpy.asarray(Image.open(path).convert('L')) - 128.) / 128.
+    return (numpy.asarray(Image.open(path).convert('L')) - 128.) / 256.
 
 
 def save_frame(width, height):
@@ -72,6 +67,131 @@ def save_frame(width, height):
     Image.fromstring(mode="RGB", size=(width, height), data=pixels
                      ).transpose(Image.FLIP_TOP_BOTTOM
                                  ).save(now().strftime('frame-%Y%m%d-%H%M%S.%f.png'))
+
+
+class Simulator(object):
+    def __init__(self, opts, args):
+        self.opts = opts
+        self.updates = -1
+        self.errors = collections.deque(maxlen=10)
+
+        self.devset = []
+        for _ in range(min(len(args) // 2, 10)):
+            arg = args[rng.randint(len(args))]
+            self.devset.append(load_image(arg))
+            args.remove(arg)
+        self.images = [load_image(arg) for arg in args]
+
+        self.codebook = lmj.pursuit.SpatialCodebook(
+            opts.rows * opts.cols, (20, 20))
+
+        self.trainer = lmj.pursuit.SpatialTrainer(
+            self.codebook,
+            min_coeff=opts.min_coeff,
+            max_num_coeffs=opts.max_num_coeffs,
+            min_activity_ratio=opts.min_activity_ratio,
+            padding=opts.padding,
+            grow=opts.grow,
+            shrink=opts.shrink,
+            )
+
+        w = max(x.shape[0] for x in self.images)
+        h = max(x.shape[1] for x in self.images)
+        self.source = numpy.zeros((w, h), 'f')
+        self.reconst = numpy.zeros((w, h), 'f')
+        self.filters = numpy.zeros((opts.rows, opts.cols, 20, 20), 'f')
+        self.features = numpy.zeros((opts.rows, opts.cols, w, h), 'f')
+
+        kwargs = dict(cmap=glumpy.colormap.Grey, vmin=-0.5, vmax=0.5)
+        self.source_image = glumpy.Image(self.source, **kwargs)
+        self.reconst_image = glumpy.Image(self.reconst, **kwargs)
+        self.filter_images = [
+            [glumpy.Image(f, vmin=-0.2, vmax=0.2) for f in fs]
+            for fs in self.filters]
+        self.feature_images = [
+            [glumpy.Image(f, vmin=0, vmax=3) for f in fs]
+            for fs in self.features]
+
+        self.iterator = self.learn_forever()
+
+    def learn(self):
+        self.source[:] = 0.
+        self.reconst[:] = 0.
+        self.features[:] = 0.
+
+        pixels = self.images[rng.randint(len(self.images))].copy()
+        w, h = pixels.shape
+        self.source[:w, :h] += pixels
+
+        grad = [numpy.zeros_like(w) for w in self.codebook.filters]
+        activity = numpy.zeros((len(grad), ), float)
+
+        for index, coeff, (x, y) in self.codebook.iterencode(
+                pixels,
+                self.opts.min_coeff,
+                self.opts.max_num_coeffs):
+            w, h = self.codebook.filters[index].shape[:2]
+
+            grad[index] += coeff * pixels[x:x+w, y:y+h]
+            activity[index] += coeff
+
+            a, b = numpy.unravel_index(index, (opts.rows, opts.cols))
+            self.features[a, b, x:x+w, y:y+h] += coeff
+
+            self.reconst[x:x+w, y:y+h] += coeff * self.codebook.filters[index]
+
+            yield
+
+        self.trainer.apply_gradient(grad, activity, self.opts.learning_rate)
+
+        w = max(w.shape[0] for w in self.codebook.filters)
+        h = max(w.shape[1] for w in self.codebook.filters)
+
+        if w > self.filters.shape[2] or h > self.filters.shape[3]:
+            x = max(w * 1.5, h * 1.5)
+            self.filters = numpy.zeros((self.opts.rows, self.opts.cols, x, x), 'f')
+            self.filter_images = [
+                [glumpy.Image(f, vmin=-0.2, vmax=0.2) for f in fs]
+                for fs in self.filters]
+
+        self.filters[:] = 0.
+        for r in range(self.opts.rows):
+            for c in range(self.opts.cols):
+                f = self.codebook.filters[r * self.opts.cols + c]
+                x, y = f.shape
+                a = (self.filters.shape[2] - x) // 2
+                b = (self.filters.shape[3] - y) // 2
+                self.filters[r, c, a:a+x, b:b+y] += f
+
+        [[f.update() for f in fs] for fs in self.filter_images]
+
+    def learn_forever(self):
+        while True:
+            for _ in self.learn():
+                yield
+            self.errors.append(abs(self.source - self.reconst).sum())
+            logging.error('error %d', sum(self.errors) / max(1, len(self.errors)))
+
+    def draw(self, W, H, p=4):
+        self.source_image.update()
+        self.reconst_image.update()
+
+        [[f.update() for f in fs] for fs in self.feature_images]
+
+        w = int(float(W - p) / (2 * self.opts.cols))
+        h = int(float(H - p) / (2 * self.opts.rows))
+        self.source_image.blit(
+            p, h * self.opts.rows + p, w * self.opts.cols - p, h * self.opts.rows - p)
+        self.reconst_image.blit(w * self.opts.cols + p,
+                                h * self.opts.rows + p,
+                                w * self.opts.cols - p,
+                                h * self.opts.rows - p)
+        for r in range(self.opts.rows):
+            fs = self.filter_images[r]
+            Fs = self.feature_images[r]
+            for c in range(self.opts.cols):
+                fs[c].blit(w * c + p, h * r + p, w - p, h - p)
+                Fs[c].blit(w * (c + self.opts.cols) + p, h * r + p, w - p, h - p)
 
 
 if __name__ == '__main__':
@@ -86,154 +206,31 @@ if __name__ == '__main__':
 
     assert len(args) > 1, 'Usage: image.py FILE...'
 
-    updates = -1
-    batches = 0.
-    devset = []
-    for _ in range(min(len(args) // 2, 10)):
-        arg = args[rng.randint(len(args))]
-        devset.append(load_image(arg))
-        args.remove(arg)
-    images = [load_image(arg) for arg in args]
-    errors = collections.deque(maxlen=10)
-
-    codebook = (opts.model and
-            pickle.load(open(opts.model, 'rb')) or
-            lmj.pursuit.SpatialCodebook(opts.rows * opts.cols, (20, 20)))
-
-    trainer = lmj.pursuit.SpatialTrainer(
-        codebook,
-        min_coeff=opts.min_coeff,
-        max_num_coeffs=opts.max_num_coeffs,
-        momentum=opts.momentum,
-        min_activity_ratio=opts.min_activity_ratio,
-        l1=opts.l1,
-        l2=opts.l2,
-        padding=opts.padding,
-        grow=opts.grow,
-        shrink=opts.shrink,
-        )
-
-    width = max(x.shape[0] for x in images)
-    height = max(x.shape[1] for x in images)
-
-    source = numpy.zeros((width, height), 'f')
-    reconst = numpy.zeros((width, height), 'f')
-    filters = numpy.zeros((opts.rows, opts.cols, 20, 20), 'f')
-    features = numpy.zeros((opts.rows, opts.cols, width, height), 'f')
-
-    kwargs = dict(cmap=glumpy.colormap.Grey, vmin=-1, vmax=1)
-    _source = glumpy.Image(source, **kwargs)
-    _reconst = glumpy.Image(reconst, **kwargs)
-    _filters = [[glumpy.Image(f, vmin=-0.2, vmax=0.2) for f in fs] for fs in filters]
-    _features = [[glumpy.Image(f, vmin=0, vmax=3) for f in fs] for fs in features]
-
-    W = 60 * (2 * opts.cols + 1) + 4
-    H = 60 * opts.rows + 4
-
-    win = glumpy.Window(W, H)
-
-    def update():
-        global batches, filters, _filters
-
-        batches += 1
-        batch = [images[rng.randint(len(images))] for _ in range(opts.batch_size)]
-
-        start = time.time()
-        grad = None
-        activity = None
-        for pixels in batch:
-            #trainer.learn(pixels, opts.learning_rate)
-            g, a = trainer.calculate_gradient(pixels.copy())
-            if grad is None:
-                grad = g
-                activity = a
-            else:
-                for t, s in zip(grad, g):
-                    t += s
-                activity += a
-        alpha = opts.learning_rate * opts.learning_decay ** batches
-        trainer.apply_gradient(grad, activity, learning_rate=alpha)
-        logging.info('processed batch in %dms', 1000 * (time.time() - start))
-
-        enc = codebook.encode(pixels.copy(),
-                              max_num_coeffs=opts.max_num_coeffs,
-                              min_coeff=opts.min_coeff)
-
-        estimate = codebook.decode(enc, pixels.shape)
-
-        errors.append(abs(pixels - estimate).sum())
-        logging.error('%d<%.3g>: error %d', batches, alpha, sum(errors) / max(1, len(errors)))
-
-        w, h = pixels.shape
-
-        source[:] = 0.
-        source[:w, :h] = pixels
-        _source.update()
-
-        reconst[:] = 0.
-        reconst[:w, :h] = estimate
-        _reconst.update()
-
-        features[:] = 0
-        for i, c, (x, y) in enc:
-            w, h = codebook.filters[i].shape
-            a, b = numpy.unravel_index(i, (opts.rows, opts.cols))
-            features[a, b, x:x+w, y:y+h] += c
-        [[f.update() for f in fs] for fs in _features]
-
-        filters[:] = 0
-        for r in range(opts.rows):
-            for c in range(opts.cols):
-                f = codebook.filters[r * opts.cols + c]
-                x, y = f.shape
-                a = (filters.shape[2] - x) // 2
-                b = (filters.shape[3] - y) // 2
-                filters[r, c, a:a+x, b:b+y] += f
-        [[f.update() for f in fs] for fs in _filters]
-
-    def evaluate():
-        for t, pixels in enumerate(devset):
-            estimate = trainer.reconstruct(pixels.copy())
-            errors.append(abs(pixels - estimate).sum())
-        logging.error('error %d', sum(errors) / max(1, len(errors)))
+    win = glumpy.Window()
+    sim = Simulator(opts, args)
 
     @win.event
     def on_draw():
         win.clear()
-        W, H = win.get_size()
-        p = 4
-        w = int(float(W - p) / (2 * opts.cols))
-        h = int(float(H - p) / (2 * opts.rows))
-        _source.blit(p, h * opts.rows + p, w * opts.cols - p, h * opts.rows - p)
-        _reconst.blit(w * opts.cols + p, h * opts.rows + p, w * opts.cols - p, h * opts.rows - p)
-        for r in range(opts.rows):
-            fs = _filters[r]
-            Fs = _features[r]
-            for c in range(opts.cols):
-                fs[c].blit(w * c + p, h * r + p, w - p, h - p)
-                Fs[c].blit(w * (c + opts.cols) + p, h * r + p, w - p, h - p)
+        sim.draw(*win.get_size())
 
     @win.event
     def on_idle(dt):
-        global updates
         win.draw()
-        if updates != 0:
-            updates -= 1
-            update()
+        if sim.updates != 0:
+            sim.updates -= 1
+            next(sim.iterator)
             if opts.save_frames:
-                save_frame(W, H)
+                save_frame(*win.get_size())
 
     @win.event
     def on_key_press(key, modifiers):
-        global updates
         if key == glumpy.key.ESCAPE:
             sys.exit()
-        if key == glumpy.key.S:
-            pickle.dump(book, open(now().strftime('pursuit-%Y%m%d-%H%M%S.p'), 'wb'))
         if key == glumpy.key.SPACE:
-            updates = updates == 0 and -1 or 0
+            sim.updates = sim.updates == 0 and -1 or 0
         if key == glumpy.key.ENTER:
-            if updates >= 0:
-                updates = 1
+            if sim.updates >= 0:
+                sim.updates = 1
 
     win.mainloop()
