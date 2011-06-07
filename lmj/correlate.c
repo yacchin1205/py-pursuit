336eb6dfa24be293c7cf3d69ceca8b180ca12418
Leif Johnson <leif@leifjohnson.net>
2011-06-07 18:32:50 -0500

    Remove multithreading from the C correlation functions: most correlation ops do not last long enough to use threads effectively.

--- lmj/correlate.c
+++ lmj/correlate.c
@@ -27,92 +27,17 @@
  * small number of common cases, namely 1d and 2d arrays consisting of
  * double-precision values. The function signature is always
  *
- *   correlateX(s, f, r, concurrency=2)
- *
- * Where "concurrency" determines the number of CPU threads that will be used to
- * do the work in parallel.
+ *   correlateX(s, f, r)
  *
  * The functions here are intended to be a replacement for equivalent calls to
  * scipy.signal.correlate(signal, filter, 'valid'). They provide a speedup of
- * approximately 10 * concurrency over the scipy implementation, probably due to
- * the multithreading and the forced dimension constraints.
+ * approximately 10x over the scipy implementation.
  */
 
 #include <Python.h>
-#include <pthread.h>
 #include <numpy/arrayobject.h>
 
 static char err[200];
-
-static int _concurrency;
-
-struct ThreadThunk {
-    PyArrayObject *signal;
-    PyArrayObject *filter;
-    PyArrayObject *result;
-    int concurrency;
-    int me;
-};
-
-static void concurrent_correlate(
-        PyArrayObject *signal, PyArrayObject *filter, PyArrayObject *result,
-        void *(* multiply)(void *),
-        int concurrency) {
-    int t;
-
-    if (concurrency < 1) concurrency = _concurrency;
-
-    pthread_attr_t joinable;
-    pthread_attr_init(&joinable);
-    pthread_attr_setdetachstate(&joinable, PTHREAD_CREATE_JOINABLE);
-
-    pthread_t *threads = calloc(concurrency, sizeof(pthread_t));
-    struct ThreadThunk *thunks = calloc(concurrency, sizeof(struct ThreadThunk));
-    for (t = 0; t < concurrency; ++t) {
-        thunks[t].signal = signal;
-        thunks[t].filter = filter;
-        thunks[t].result = result;
-        thunks[t].concurrency = concurrency;
-        thunks[t].me = t;
-        pthread_create(&threads[t], &joinable, multiply, (void *) &thunks[t]);
-    }
-
-    pthread_attr_destroy(&joinable);
-
-    for (t = 0; t < concurrency; ++t)
-        pthread_join(threads[t], NULL);
-
-    free(threads);
-    free(thunks);
-}
-
-static void *multiply1d(void *data) {
-    struct ThreadThunk *thunk = (struct ThreadThunk *) data;
-    PyArrayObject *signal = thunk->signal;
-    PyArrayObject *filter = thunk->filter;
-    PyArrayObject *result = thunk->result;
-    const int concurrency = thunk->concurrency;
-    const int me = thunk->me;
-
-    const int slens = signal->strides[0] / sizeof(double);
-    const int flens = filter->strides[0] / sizeof(double);
-    const int flend = filter->dimensions[0];
-    const int rlend = result->dimensions[0];
-
-    double *s = (double *) signal->data;
-    double *f = (double *) filter->data;
-    double *r = (double *) result->data;
-    int i, a;
-    double acc;
-    for (i = me * rlend / concurrency; i < (me + 1) * rlend / concurrency; ++i) {
-        acc = 0.0;
-        for (a = 0; a < flend; ++a)
-            acc += f[a * flens] * s[(i + a) * slens];
-        r[i] = acc;
-    }
-
-    pthread_exit(NULL);
-}
 
 /* void correlate1d(1D-array signal, 1D-array filter, 1D-array *result)
  *
@@ -121,12 +46,10 @@ static void *multiply1d(void *data) {
  */
 static PyObject *correlate1d(PyObject *self, PyObject *args) {
     PyArrayObject *signal, *filter, *result;
-    int concurrency = 0;
-    if (!PyArg_ParseTuple(args, "O!O!O!|i",
+    if (!PyArg_ParseTuple(args, "O!O!O!",
                           &PyArray_Type, &signal,
                           &PyArray_Type, &filter,
-                          &PyArray_Type, &result,
-                          &concurrency)) {
+                          &PyArray_Type, &result)) {
         PyErr_SetString(PyExc_ValueError, "correlate requires three ndarray arguments");
         return NULL;
     }
@@ -150,43 +73,25 @@ static PyObject *correlate1d(PyObject *self, PyObject *args) {
         PyErr_SetString(PyExc_ValueError, err);
         return NULL;
     }
-    concurrent_correlate(signal, filter, result, multiply1d, concurrency);
-    Py_RETURN_NONE;
-}
-
-static void *multiply1d_from_2d(void *data) {
-    struct ThreadThunk *thunk = (struct ThreadThunk *) data;
-    PyArrayObject *signal = thunk->signal;
-    PyArrayObject *filter = thunk->filter;
-    PyArrayObject *result = thunk->result;
-    const int concurrency = thunk->concurrency;
-    const int me = thunk->me;
 
     const int slens = signal->strides[0] / sizeof(double);
-    const int swids = signal->strides[1] / sizeof(double);
-
     const int flens = filter->strides[0] / sizeof(double);
-    const int fwids = filter->strides[1] / sizeof(double);
-
     const int flend = filter->dimensions[0];
-    const int fwidd = filter->dimensions[1];
-
     const int rlend = result->dimensions[0];
 
     double *s = (double *) signal->data;
     double *f = (double *) filter->data;
     double *r = (double *) result->data;
-    int i, a, b;
+    int i, a;
     double acc;
-    for (i = me * rlend / concurrency; i < (me + 1) * rlend / concurrency; ++i) {
+    for (i = 0; i < rlend; ++i) {
         acc = 0.0;
         for (a = 0; a < flend; ++a)
-            for (b = 0; b < fwidd; ++b)
-                acc += f[a * flens + b * fwids] * s[(i + a) * slens + b * swids];
+            acc += f[a * flens] * s[(i + a) * slens];
         r[i] = acc;
     }
 
-    pthread_exit(NULL);
+    Py_RETURN_NONE;
 }
 
 /* void correlate1d_from_2d(2D-array signal, 2D-array filter, 1D-array *result)
@@ -198,12 +103,10 @@ static void *multiply1d_from_2d(void *data) {
  */
 static PyObject *correlate1d_from_2d(PyObject *self, PyObject *args) {
     PyArrayObject *signal, *filter, *result;
-    int concurrency = 0;
-    if (!PyArg_ParseTuple(args, "O!O!O!|i",
+    if (!PyArg_ParseTuple(args, "O!O!O!",
                           &PyArray_Type, &signal,
                           &PyArray_Type, &filter,
-                          &PyArray_Type, &result,
-                          &concurrency)) {
+                          &PyArray_Type, &result)) {
         PyErr_SetString(PyExc_ValueError, "correlate requires three ndarray arguments");
         return NULL;
     }
@@ -234,26 +137,12 @@ static PyObject *correlate1d_from_2d(PyObject *self, PyObject *args) {
         PyErr_SetString(PyExc_ValueError, err);
         return NULL;
     }
-    concurrent_correlate(signal, filter, result, multiply1d_from_2d, concurrency);
-    Py_RETURN_NONE;
-}
-
-static void *multiply2d(void *data) {
-    struct ThreadThunk *thunk = (struct ThreadThunk *) data;
-    PyArrayObject *signal = thunk->signal;
-    PyArrayObject *filter = thunk->filter;
-    PyArrayObject *result = thunk->result;
-    const int concurrency = thunk->concurrency;
-    const int me = thunk->me;
 
     const int slens = signal->strides[0] / sizeof(double);
     const int swids = signal->strides[1] / sizeof(double);
 
     const int flens = filter->strides[0] / sizeof(double);
     const int fwids = filter->strides[1] / sizeof(double);
-
-    const int rlens = result->strides[0] / sizeof(double);
-    const int rwids = result->strides[1] / sizeof(double);
 
     const int flend = filter->dimensions[0];
     const int fwidd = filter->dimensions[1];
@@ -263,20 +152,17 @@ static void *multiply2d(void *data) {
     double *s = (double *) signal->data;
     double *f = (double *) filter->data;
     double *r = (double *) result->data;
-    int i, j, a, b;
+    int i, a, b;
     double acc;
-    for (i = me * rlend / concurrency; i < (me + 1) * rlend / concurrency; ++i) {
-        for (j = 0; j < result->dimensions[1]; ++j) {
-            acc = 0.0;
-            for (a = 0; a < flend; ++a)
-                for (b = 0; b < fwidd; ++b)
-                    acc += f[a * flens + b * fwids] *
-                        s[(i + a) * slens + (j + b) * swids];
-            r[i * rlens + j * rwids] = acc;
-        }
+    for (i = 0; i < rlend; ++i) {
+        acc = 0.0;
+        for (a = 0; a < flend; ++a)
+            for (b = 0; b < fwidd; ++b)
+                acc += f[a * flens + b * fwids] * s[(i + a) * slens + b * swids];
+        r[i] = acc;
     }
 
-    pthread_exit(NULL);
+    Py_RETURN_NONE;
 }
 
 /* void correlate2d(2D-array signal, 2D-array filter, 2D-array *result)
@@ -287,12 +173,10 @@ static void *multiply2d(void *data) {
  */
 static PyObject *correlate2d(PyObject *self, PyObject *args) {
     PyArrayObject *signal, *filter, *result;
-    int concurrency = 0;
-    if (!PyArg_ParseTuple(args, "O!O!O!|i",
+    if (!PyArg_ParseTuple(args, "O!O!O!",
                           &PyArray_Type, &signal,
                           &PyArray_Type, &filter,
-                          &PyArray_Type, &result,
-                          &concurrency)) {
+                          &PyArray_Type, &result)) {
         PyErr_SetString(PyExc_ValueError, "correlate requires three ndarray arguments");
         return NULL;
     }
@@ -320,25 +204,12 @@ static PyObject *correlate2d(PyObject *self, PyObject *args) {
         PyErr_SetString(PyExc_ValueError, err);
         return NULL;
     }
-    concurrent_correlate(signal, filter, result, multiply2d, concurrency);
-    Py_RETURN_NONE;
-}
-
-static void *multiply2d_from_rgb(void *data) {
-    struct ThreadThunk *thunk = (struct ThreadThunk *) data;
-    PyArrayObject *signal = thunk->signal;
-    PyArrayObject *filter = thunk->filter;
-    PyArrayObject *result = thunk->result;
-    const int concurrency = thunk->concurrency;
-    const int me = thunk->me;
 
     const int slens = signal->strides[0] / sizeof(double);
     const int swids = signal->strides[1] / sizeof(double);
-    const int sdeps = signal->strides[2] / sizeof(double);
 
     const int flens = filter->strides[0] / sizeof(double);
     const int fwids = filter->strides[1] / sizeof(double);
-    const int fdeps = filter->strides[2] / sizeof(double);
 
     const int rlens = result->strides[0] / sizeof(double);
     const int rwids = result->strides[1] / sizeof(double);
@@ -348,37 +219,23 @@ static void *multiply2d_from_rgb(void *data) {
 
     const int rlend = result->dimensions[0];
 
-    /* TODO: see if we can incorporate a perceptually motivated "distance" for
-     * RGB like this one from http://www.compuphase.com/cmetric.htm :
-     *
-     *   r = (c1[r] + c2[r]) / 2
-     *   d = c1 - c2
-     *   dist = sqrt((2 + r / 256) * d[r] * d[r] +
-     *               4 * d[g] * d[g] +
-     *               (2 + (255 - r) / 256) * d[b] * d[b])
-     */
     double *s = (double *) signal->data;
     double *f = (double *) filter->data;
     double *r = (double *) result->data;
     int i, j, a, b;
     double acc;
-    for (i = me * rlend / concurrency; i < (me + 1) * rlend / concurrency; ++i) {
+    for (i = 0; i < rlend; ++i) {
         for (j = 0; j < result->dimensions[1]; ++j) {
             acc = 0.0;
             for (a = 0; a < flend; ++a)
                 for (b = 0; b < fwidd; ++b)
-                    acc +=
-                        f[a * flens + b * fwids] *
-                          s[(i + a) * slens + (j + b) * swids] +
-                        f[a * flens + b * fwids + fdeps] *
-                          s[(i + a) * slens + (j + b) * swids + sdeps] +
-                        f[a * flens + b * fwids + 2 * fdeps] *
-                          s[(i + a) * slens + (j + b) * swids + 2 * sdeps];
+                    acc += f[a * flens + b * fwids] *
+                        s[(i + a) * slens + (j + b) * swids];
             r[i * rlens + j * rwids] = acc;
         }
     }
 
-    pthread_exit(NULL);
+    Py_RETURN_NONE;
 }
 
 /* void correlate2d_from_rgb(3D-array signal, 3D-array filter, 2D-array *result)
@@ -389,12 +246,10 @@ static void *multiply2d_from_rgb(void *data) {
  */
 static PyObject *correlate2d_from_rgb(PyObject *self, PyObject *args) {
     PyArrayObject *signal, *filter, *result;
-    int concurrency = 0;
     if (!PyArg_ParseTuple(args, "O!O!O!",
                           &PyArray_Type, &signal,
                           &PyArray_Type, &filter,
-                          &PyArray_Type, &result,
-                          &concurrency)) {
+                          &PyArray_Type, &result)) {
         PyErr_SetString(PyExc_ValueError, "correlate requires three ndarray arguments");
         return NULL;
     }
@@ -434,7 +289,53 @@ static PyObject *correlate2d_from_rgb(PyObject *self, PyObject *args) {
         PyErr_SetString(PyExc_ValueError, err);
         return NULL;
     }
-    concurrent_correlate(signal, filter, result, multiply2d_from_rgb, concurrency);
+
+    const int slens = signal->strides[0] / sizeof(double);
+    const int swids = signal->strides[1] / sizeof(double);
+    const int sdeps = signal->strides[2] / sizeof(double);
+
+    const int flens = filter->strides[0] / sizeof(double);
+    const int fwids = filter->strides[1] / sizeof(double);
+    const int fdeps = filter->strides[2] / sizeof(double);
+
+    const int rlens = result->strides[0] / sizeof(double);
+    const int rwids = result->strides[1] / sizeof(double);
+
+    const int flend = filter->dimensions[0];
+    const int fwidd = filter->dimensions[1];
+
+    const int rlend = result->dimensions[0];
+
+    /* TODO: see if we can incorporate a perceptually motivated "distance" for
+     * RGB like this one from http://www.compuphase.com/cmetric.htm :
+     *
+     *   r = (c1[r] + c2[r]) / 2
+     *   d = c1 - c2
+     *   dist = sqrt((2 + r / 256) * d[r] * d[r] +
+     *               4 * d[g] * d[g] +
+     *               (2 + (255 - r) / 256) * d[b] * d[b])
+     */
+    double *s = (double *) signal->data;
+    double *f = (double *) filter->data;
+    double *r = (double *) result->data;
+    int i, j, a, b;
+    double acc;
+    for (i = 0; i < rlend; ++i) {
+        for (j = 0; j < result->dimensions[1]; ++j) {
+            acc = 0.0;
+            for (a = 0; a < flend; ++a)
+                for (b = 0; b < fwidd; ++b)
+                    acc +=
+                        f[a * flens + b * fwids] *
+                          s[(i + a) * slens + (j + b) * swids] +
+                        f[a * flens + b * fwids + fdeps] *
+                          s[(i + a) * slens + (j + b) * swids + sdeps] +
+                        f[a * flens + b * fwids + 2 * fdeps] *
+                          s[(i + a) * slens + (j + b) * swids + 2 * sdeps];
+            r[i * rlens + j * rwids] = acc;
+        }
+    }
+
     Py_RETURN_NONE;
 }
 
@@ -454,28 +355,4 @@ PyMODINIT_FUNC
 init_correlate(void) {
     import_array();
     (void) Py_InitModule("_correlate", CorrelateMethods);
-
-    /* default to using one worker thread. :( */
-    _concurrency = 1;
-    PyObject *mod_name = PyString_FromString("multiprocessing");
-    if (mod_name != NULL) {
-        PyObject *module = PyImport_Import(mod_name);
-        if (module != NULL) {
-            PyObject *fn_name = PyString_FromString("cpu_count");
-            if (fn_name != NULL) {
-                PyObject *function = PyObject_GetAttr(module, fn_name);
-                if (function != NULL) {
-                    PyObject *result = PyObject_CallFunctionObjArgs(function, NULL);
-                    if (result != NULL) {
-                        _concurrency = (int) PyInt_AsLong(result);
-                        Py_DECREF(result);
-                    }
-                    Py_DECREF(function);
-                }
-                Py_DECREF(fn_name);
-            }
-            Py_DECREF(module);
-        }
-        Py_DECREF(mod_name);
-    }
 }
