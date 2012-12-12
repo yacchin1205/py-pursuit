063ea8fd2343f4216ad2631e1b36e98c7f9fc923
Leif Johnson <leif@leifjohnson.net>
2012-12-11 18:07:53 -0600

    Add README.md to package manifest ; update setup.py to reflect best packaging know-how to date.

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
+import os
 import setuptools
 
 setuptools.setup(
     name='lmj.pursuit',
-    version='0.3',
-    install_requires=['numpy'],
+    version='0.3.1',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
-    ext_modules=[setuptools.Extension('lmj.pursuit._correlate', sources=['lmj/pursuit/correlate.c'])],
     author='Leif Johnson',
     author_email='leif@leifjohnson.net',
     description='A library of matching pursuit implementations',
-    long_description=open('README.rst').read(),
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
     license='MIT',
+    url='http://github.com/lmjohns3/py-pursuit',
     keywords=('matching-pursuit '
               'sparse-coding '
               'compressed-sensing '
               'machine-learning'),
-    url='http://github.com/lmjohns3/py-pursuit',
+    install_requires=['numpy'],
+    ext_modules=[setuptools.Extension('lmj.pursuit._correlate', sources=['lmj/pursuit/correlate.c'])],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
